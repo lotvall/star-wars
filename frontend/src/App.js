616fe30eaf235b73f9331486b4fd373685439205
Alexander Lötvall <alexander.lotvall@gmail.com>
2019-02-07 21:25:46 +0100

    added navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import NavBar from './components/NavBar'
+
+const containerStyle = {
+  width: '100%',
+  display:'block',
+  margin: 'auto'
+}
+const client = new ApolloClient({
+  uri: '/graphql'
+})
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>Hello world</p>
-      </div>
+        <ApolloProvider client={client}>
+          <Router>
+            
+            <div className="container" style={{...containerStyle}}>
+              <NavBar/>
+              {/* <Route exact path='/' component={Launches}/>
+              <Route exact path='/launch/:flight_number' component={Launch}/> */}
+
+
+            </div>
+          </Router>
+        </ApolloProvider>
     );
   }
 }
